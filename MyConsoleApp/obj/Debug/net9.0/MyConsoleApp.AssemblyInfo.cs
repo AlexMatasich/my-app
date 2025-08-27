20cd9370ea520d44bc55432a527ffcf2dec45e3f
Alex Matasich <matasich.9@buckeyemail.osu.edu>
2025-08-27 17:31:50 -0400

    esLint Updated - Check

--- MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07e2a9460b00f24bfe48c1dd0e635dc4e1f4933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65064bf5b87769137dbdcacaa901625b40ea9e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,15 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: globals.browser 
+    },
+  rules: {
+    "no-extra-semi": "error",
+  } },
   pluginReact.configs.flat.recommended,
 ]);
