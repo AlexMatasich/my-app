d4be00fc095f8a48a6354dbcd0272de2e88855c9
Alex Matasich <matasich.9@buckeyemail.osu.edu>
2025-09-02 17:58:07 -0400

    Final changes

--- MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c07648909199b4f701a5f2d5c8c218dc73fa04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25559dd087a7b1bcb55b88ecfb1fd62a20074c0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
